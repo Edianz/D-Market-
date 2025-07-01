be77820532b1b9aef5a5109909873c4c3f1da9f5
Edianz <hounvenoudiane09@gmail.com>
2025-07-02 00:23:13 +0100

    Create Accueil.js

---
+++ Accueil.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Accueil = () => (
+  <div style={{ textAlign: "center", marginTop: 40 }}>
+    <h1>Bienvenue chez D-Market</h1>
+    <p style={{ fontSize: 24, color: "#2a9d8f", marginTop: 20 }}>
+      Avec D-Market, chaque jour a du style
+    </p>
+  </div>
+);
+
+export default Accueil;
+        
