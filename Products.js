7355e001148bb2847a044e4c2a6d9ab2d7baeaff
Edianz <hounvenoudiane09@gmail.com>
2025-07-02 00:21:32 +0100

    Create Products.js

---
+++ Products.js
@@ -0,0 +1,44 @@
+export const products = {
+  hommes: [
+    {
+      id: 1,
+      name: "T-shirt Homme",
+      price: 25,
+      images: [
+        "/images/hommes1.jpg",
+        "/images/hommes2.jpg",
+        "/images/hommes3.jpg"
+      ]
+    },
+    {
+      id: 2,
+      name: "Jean Homme",
+      price: 50,
+      images: ["/images/hommes4.jpg"]
+    }
+  ],
+  femmes: [
+    {
+      id: 3,
+      name: "Robe Femme",
+      price: 40,
+      images: ["/images/femmes1.jpg"]
+    }
+  ],
+  accessoires: [
+    {
+      id: 4,
+      name: "Sac à main",
+      price: 60,
+      images: ["/images/accessoires1.jpg"]
+    }
+  ],
+  baskets: [
+    {
+      id: 5,
+      name: "Basket Sport",
+      price: 70,
+      images: ["/images/baskets1.jpg"]
+    }
+  ]
+};
